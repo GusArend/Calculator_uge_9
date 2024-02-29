54d380e57148edfd6a6fcdcca5d103a3f1ee823a
Gustav Rasmussen <gustavarend@me.com>
2024-02-29 10:52:57 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Lommeregner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            double sum = 0;
+            string quit = "";
+            bool isCalculatorOn = true;
+
+            while (isCalculatorOn)
+            {
+                Console.Write("Would you like to Add(+), Subtract(-), Divide(/) or Multiply(*)? ");
+                string tegn = Console.ReadLine();
+                
+                if ((tegn == "+") || (tegn == "-") || (tegn == "/") || (tegn == "*"))
+                {
+                    try
+                    {
+
+                        Console.Write("Enter the first number: ");
+                        int num1 = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter the secound number: ");
+                        int num2 = int.Parse(Console.ReadLine());
+
+                        switch (tegn)
+                        {
+                            case "+":
+                                sum = calculator.Add(num1, num2);
+                                break;
+                            case "-":
+                                sum = calculator.Subtract(num1, num2);
+                                break;
+                            case "*":
+                                sum = calculator.Multiply(num1, num2);
+                                break;
+                            case "/":
+                                sum = calculator.Divide(num1, num2);
+                                break;
+                        }
+
+
+                        Console.WriteLine($"{num1} {tegn} {num2} = {sum}");
+
+                        Console.Write("Press enter to calculate again or X to quit ");
+                        quit = Console.ReadLine();
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Enter only whole numbers.");
+                        
+                        Console.Write("Press enter to calculate again or X to quit ");
+                        quit = Console.ReadLine();
+                    }
+                } else
+                {
+                    Console.WriteLine("Only enter +, -, / or *");
+                }
+
+
+                if (quit == "x") isCalculatorOn = false;
+                Console.Clear();
+            }
+            
+        }
+    }
+}
